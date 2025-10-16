09c691c88c3a5ffc13379ebf3a58bc7e95dcea43
TheBlueF0x <edwardcooke555@outlook.com>
2025-10-16 16:43:48 +0100

    python3 miniproject

---
+++ Python3/markovifyText.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import markovify
+
+# Get raw text as string.
+with open("Alice.txt") as f:
+    text = f.read()
+
+# Build the model.
+text_model = markovify.Text(text)
+
+# Print five randomly-generated sentences
+for i in range(5):
+    print(text_model.make_sentence())
+
+# Print three randomly-generated sentences of no more than 280 characters
+for i in range(3):
+    print(text_model.make_short_sentence(280)) 
